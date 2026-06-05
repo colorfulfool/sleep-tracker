540e4134854fd0e42bf6dfd02a3dad58a616bbbf
Anton Khamets <anton.khamets@hey.com>
2026-06-05 11:11:35 +0500

    Fetch only ocne

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script module>
 	import Graph from '$lib/graph.svelte';
-	import { onMount } from 'svelte';
 
+	/** @param {string} url */
 	async function readResponse(url) {
 		const resp = await fetch(url, { cache: 'no-store' });
 		const text = await resp.text();
 		return text.split('\n');
 	}
 
-	/**
-	 * @param {Array<string>} rows
-	 */
-	function* generator(rows) {
+	/** @param {Array<string>} rows */
+	function* everyDay(rows) {
 		let currentDate;
 		while (true) {
 			const currentRow = rows.shift()?.split(',');
@@ -35,33 +33,10 @@
 	}
 </script>
 
-<script>
-	/** @type {string[] | undefined} */
-	let data = $state(undefined);
-
-	const fetchData = async () => {
-		data = await readResponse('/data.csv');
-	};
-
-	const onVisible = () => {
-		if (document.visibilityState === 'visible') fetchData();
-	};
-
-	onMount(() => {
-		fetchData();
-		document.addEventListener('visibilitychange', onVisible);
-		window.addEventListener('focus', fetchData);
-		return () => {
-			document.removeEventListener('visibilitychange', onVisible);
-			window.removeEventListener('focus', fetchData);
-		};
-	});
-</script>
-
 <table>
 	<tbody>
-		{#if data}
-			{#each generator([...data]) as row}
+		{#await readResponse('/data.csv') then data}
+			{#each everyDay([...data]) as row}
 				<tr>
 					<td>{row[0]}</td>
 					{#if row[1] && row[2]}
@@ -69,7 +44,7 @@
 					{/if}
 				</tr>
 			{/each}
-		{/if}
+		{/await}
 	</tbody>
 </table>
 
