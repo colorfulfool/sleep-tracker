0d34f96f4d5df1746292b7a8ee2287e80a6ab3ab
Anton Khamets <anton.khamets@hey.com>
2026-06-05 11:54:29 +0500

    Extract table

---
+++ src/lib/table.svelte
@@ -0,0 +1,52 @@
+<script module>
+	/** @param {Array<string>} rows */
+	function* everyDay(rows) {
+		let currentDate;
+		while (true) {
+			const currentRow = rows.shift()?.split(',');
+
+			if (!currentRow) {
+				return;
+			}
+
+			if (!currentDate) {
+				currentDate = new Date(currentRow[0]);
+			}
+
+			while (new Date(currentRow[0]).getTime() - currentDate.getTime() > 1000 * 60 * 60 * 24) {
+				currentDate.setDate(currentDate.getDate() + 1);
+				yield [currentDate.toISOString().split('T')[0], '', ''];
+			}
+
+			currentDate = new Date(currentRow[0]);
+			yield currentRow;
+		}
+	}
+</script>
+
+<script>
+	import Graph from './graph.svelte';
+
+	/** @type {{ data: string[] }} */
+	let { data } = $props();
+</script>
+
+<table>
+	<tbody>
+		{#each everyDay([...data]) as row}
+			<tr>
+				<td>{row[0]}</td>
+				{#if row[1] && row[2]}
+					<td><Graph startTime={row[1]} endTime={row[2]} /></td>
+				{/if}
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table td {
+		padding: 8px;
+		font-family: sans-serif;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script module>
-	import Graph from '$lib/graph.svelte';
+	import Table from '$lib/table.svelte';
 
 	/** @param {string} url */
 	async function readResponse(url) {
@@ -7,50 +7,18 @@
 		const text = await resp.text();
 		return text.split('\n');
 	}
-
-	/** @param {Array<string>} rows */
-	function* everyDay(rows) {
-		let currentDate;
-		while (true) {
-			const currentRow = rows.shift()?.split(',');
-
-			if (!currentRow) {
-				return;
-			}
-
-			if (!currentDate) {
-				currentDate = new Date(currentRow[0]);
-			}
-
-			while (new Date(currentRow[0]).getTime() - currentDate.getTime() > 1000 * 60 * 60 * 24) {
-				currentDate.setDate(currentDate.getDate() + 1);
-				yield [currentDate.toISOString().split('T')[0], '', ''];
-			}
-
-			currentDate = new Date(currentRow[0]);
-			yield currentRow;
-		}
-	}
 </script>
 
-<table>
-	<tbody>
-		{#await readResponse('/data.csv') then data}
-			{#each everyDay([...data]) as row}
-				<tr>
-					<td>{row[0]}</td>
-					{#if row[1] && row[2]}
-						<td><Graph startTime={row[1]} endTime={row[2]} /></td>
-					{/if}
-				</tr>
-			{/each}
-		{/await}
-	</tbody>
-</table>
+<div style={`flex-direction: ${'column'}`}>
+	{#await readResponse('/data.csv') then data}
+		<Table {data} />
+	{/await}
+</div>
 
 <style>
-	table td {
-		padding: 8px;
-		font-family: sans-serif;
+	div {
+		display: flex;
+		gap: 8px;
+		align-items: start;
 	}
 </style>
