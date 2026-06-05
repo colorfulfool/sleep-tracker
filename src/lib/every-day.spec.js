5e1e038123b7b4ec5afb5812ca75fa856d54bd8f
Anton Khamets <anton.khamets@hey.com>
2026-06-05 13:22:12 +0500

    Add failing test

--- src/lib/every-day.spec.js
+++ src/lib/every-day.spec.js
@@ -13,5 +13,17 @@ describe('everyDay', () => {
     expect(gen.next().value).toEqual(["2026-03-03", "", ""])
     expect(gen.next().value).toEqual(["2026-03-04", "7:24", "12:55"])
     expect(gen.next().value).toEqual(["2026-03-05", "6:44", "14:06"])
+    expect(gen.next().value).toBeFalsy()
+  });
+
+  it('adds missing first day', () => {
+    const gen = everyDay([
+      "2026-03-02,6:26,16:14",
+      "2026-03-03,7:24,12:55",
+    ]);
+    expect(gen.next().value).toEqual(["2026-03-01", "", ""])
+    expect(gen.next().value).toEqual(["2026-03-02", "6:26", "16:14"])
+    expect(gen.next().value).toEqual(["2026-03-03", "7:24", "12:55"])
+    expect(gen.next().value).toBeFalsy()
   });
 });
